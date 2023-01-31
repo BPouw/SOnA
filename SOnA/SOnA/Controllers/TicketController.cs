1071e6769d714eae202320a1d14f3792c0f09ae8
TheElementalDragon <60363527+TheElementalDragon@users.noreply.github.com>
2023-01-31 12:19:34 +0100

    added vscode stuff

---
+++ SOnA/SOnA/Controllers/TicketController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SOnA.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TicketController : ControllerBase
+{
+	[HttpGet(Name = "GetWeatherForecast")]
+	public IEnumerable<WeatherForecast> Get()
+	{
+		return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+		})
+		.ToArray();
+	}
+}
+
