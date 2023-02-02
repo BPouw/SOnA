83babf35eb9bebcad3e769e3ce1532ed4ea87db8
Boris Pouw <74407773+BPouw@users.noreply.github.com>
2023-02-02 16:40:23 +0100

    fixed fakes

--- SOnA/Test/Fake.cs
+++ SOnA/Test/Fake.cs
@@ -5,13 +5,14 @@ namespace Test
 {
 	public class Fake
 	{
-
+		// movie
 		public Movie GetSomeMovie()
 		{
 			return new Movie("James Bond");
 
 		}
 
+		//movie screening
 		public MovieScreening GetWeekdayMovieScreening()
 		{
 			Movie mov = GetSomeMovie();
@@ -24,58 +25,115 @@ namespace Test
 			return new MovieScreening(new DateTime(2023, 2, 5), 10, mov);
 		}
 
-		public MovieTicket GetPremiumMovieTicket()
+		// movie ticket
+		public MovieTicket GetPremiumWeekdayMovieTicket()
 		{
 			MovieScreening movieScreening = GetWeekdayMovieScreening();
 			return new MovieTicket(12, 10, true, movieScreening);
 		}
 
-		public MovieTicket GetDefaultMovieTicket()
+		public MovieTicket GetDefaultWeekdayMovieTicket()
+		{
+			MovieScreening movieScreening = GetWeekdayMovieScreening();
+			return new MovieTicket(12, 10, false, movieScreening);
+		}
+
+		public MovieTicket GetPremiumWeekendMovieTicket()
+		{
+			MovieScreening movieScreening = GetWeekendMovieScreening();
+			return new MovieTicket(12, 10, true, movieScreening);
+		}
+
+		public MovieTicket GetDefaultWeekendMovieTicket()
 		{
 			MovieScreening movieScreening = GetWeekendMovieScreening();
 			return new MovieTicket(12, 10, false, movieScreening);
 		}
 
-		public Order GetPremiumStudentOrder()
+		// orders
+		public Order GetPremiumStudentWeekendOrder()
 		{
 			List<MovieTicket> movieTickets = new List<MovieTicket>();
-			movieTickets.Add(GetDefaultMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
 
 			return new Order(1, true, movieTickets);
 		}
 
-		public Order GetPremiumDefaultOrder()
+		public Order GetPremiumDefaultWeekendOrder()
 		{
 			List<MovieTicket> movieTickets = new List<MovieTicket>();
-			movieTickets.Add(GetPremiumMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
 
 			return new Order(1, true, movieTickets);
 		}
 
-		public Order GetGroupDefaultOrder()
+		public Order GetPremiumStudentWeekdayOrder()
 		{
 			List<MovieTicket> movieTickets = new List<MovieTicket>();
-			movieTickets.Add(GetDefaultMovieTicket());
-			movieTickets.Add(GetDefaultMovieTicket());
-			movieTickets.Add(GetDefaultMovieTicket());
-			movieTickets.Add(GetDefaultMovieTicket());
-			movieTickets.Add(GetDefaultMovieTicket());
-			movieTickets.Add(GetDefaultMovieTicket());
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+
+			return new Order(1, true, movieTickets);
+		}
+
+		public Order GetPremiumDefaultWeekdayOrder()
+		{
+			List<MovieTicket> movieTickets = new List<MovieTicket>();
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+
+			return new Order(1, true, movieTickets);
+		}
+
+		public Order GetGroupDefaultWeekendOrder()
+		{
+			List<MovieTicket> movieTickets = new List<MovieTicket>();
+			movieTickets.Add(GetDefaultWeekendMovieTicket());
+			movieTickets.Add(GetDefaultWeekendMovieTicket());
+			movieTickets.Add(GetDefaultWeekendMovieTicket());
+			movieTickets.Add(GetDefaultWeekendMovieTicket());
+			movieTickets.Add(GetDefaultWeekendMovieTicket());
+			movieTickets.Add(GetDefaultWeekendMovieTicket());
 
 			return new Order(1, false, movieTickets);
 		}
 
-		public Order GetGroupPremiumOrder()
+		public Order GetGroupPremiumWeekendOrder()
 		{
 			List<MovieTicket> movieTickets = new List<MovieTicket>();
-			movieTickets.Add(GetPremiumMovieTicket());
-			movieTickets.Add(GetPremiumMovieTicket());
-			movieTickets.Add(GetPremiumMovieTicket());
-			movieTickets.Add(GetPremiumMovieTicket());
-			movieTickets.Add(GetPremiumMovieTicket());
-			movieTickets.Add(GetPremiumMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
+			movieTickets.Add(GetPremiumWeekendMovieTicket());
 
 			return new Order(1, false, movieTickets);
+		}
+
+		public Order GetGroupDefaultWeekdayOrder()
+		{
+			List<MovieTicket> movieTickets = new List<MovieTicket>();
+			movieTickets.Add(GetDefaultWeekdayMovieTicket());
+			movieTickets.Add(GetDefaultWeekdayMovieTicket());
+			movieTickets.Add(GetDefaultWeekdayMovieTicket());
+			movieTickets.Add(GetDefaultWeekdayMovieTicket());
+			movieTickets.Add(GetDefaultWeekdayMovieTicket());
+			movieTickets.Add(GetDefaultWeekdayMovieTicket());
+
+			return new Order(1, false, movieTickets);
+		}
+
+		public Order GetGroupPremiumWeekdayOrder()
+		{
+			List<MovieTicket> movieTickets = new List<MovieTicket>();
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+			movieTickets.Add(GetPremiumWeekdayMovieTicket());
+
+			return new Order(1, false, movieTickets);
+
 		}
 
 
